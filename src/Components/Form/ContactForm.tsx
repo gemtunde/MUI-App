65669bc6e8180e4b92af771616fa6b3bf95d761e
gemtunde <gemtunde@gmail.com>
2023-11-02 10:34:03 +0100

    MUI architecture

---
+++ src/Components/Form/ContactForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ContactForm = (props: Props) => {
+  return <div style={{ backgroundColor: "red" }}>ContactForm</div>;
+};
+
+export default ContactForm;
---
+++ src/Components/Scaffold/NavDrawer.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  AppBar,
+  Drawer,
+  List,
+  ListItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import ContactForm from "../Form/ContactForm";
+import ContactCardGrid from "../Grid/ContactCardGrid";
+import ContactTable from "../Table/ContactTable";
+import ContactDataGrid from "../DataGrid/ContactDataGrid";
+import { Theme, useTheme } from "@mui/material/styles";
+
+type Props = {};
+
+const drawerWidth = 240;
+
+const themedStyles = (theme: Theme) => {
+  return {
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+  };
+};
+
+const simpleStyles = {
+  drawer: {
+    width: drawerWidth,
+    "& .MuiBackdrop-root": {
+      display: "none",
+    },
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    backgroundColor: "rgba(120,120,120, 0.2)",
+  },
+  content: {
+    marginLeft: drawerWidth,
+    padding: 5,
+  },
+};
+
+const NavDrawer = (props: Props) => {
+  const theme = useTheme();
+  return (
+    <BrowserRouter>
+      <div>
+        <AppBar position="fixed" sx={themedStyles(theme).appBar}>
+          <Toolbar>
+            <Typography variant="h6" noWrap>
+              {" "}
+              MUI App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="temporary"
+          open={true}
+          sx={simpleStyles.drawer}
+          PaperProps={{
+            sx: simpleStyles.drawer,
+            elevation: 9,
+          }}
+        >
+          <List>
+            <Toolbar />
+            {[
+              { text: "Input Form", route: "/form" },
+              { text: "Contact Card Grid", route: "/grid" },
+              { text: "Contact Table", route: "/table" },
+              { text: "Contact Data Grid", route: "/datagrid" },
+            ].map((nav) => (
+              <ListItem key={nav.text}>
+                <Link to={nav.route}>{nav.text}</Link>
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+        <main style={simpleStyles.content}>
+          <Toolbar />
+          <Routes>
+            <Route path="/" element={<ContactForm />} />
+            <Route path="/form" element={<ContactForm />} />
+            <Route path="/grid" element={<ContactCardGrid />} />
+            <Route path="/table" element={<ContactTable />} />
+            <Route path="/datagrid" element={<ContactDataGrid />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default NavDrawer;
