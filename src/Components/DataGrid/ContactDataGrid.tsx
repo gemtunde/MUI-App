b740b21384232dc8194f640d472d021149e75a59
gemtunde <gemtunde@gmail.com>
2023-11-08 10:48:44 +0100

    Advance mui grid table filter and sorting

--- src/Components/DataGrid/ContactDataGrid.tsx
+++ src/Components/DataGrid/ContactDataGrid.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
+import { DataGrid, GridRenderCellParams, GridToolbar } from "@mui/x-data-grid";
 import { contactData } from "../../Data/ContactData";
 import { useTheme, Theme } from "@mui/material/styles";
+import { Box, Button } from "@mui/material";
 //import { Theme } from "@emotion/react";
 
 type Props = {};
@@ -9,13 +10,46 @@ type Props = {};
 const ContactDataGrid = (props: Props) => {
   const theme = useTheme();
 
+  const handlePrintClick = (cellValues: GridRenderCellParams<string>) => {
+    console.log(cellValues);
+  };
+
+  const dataGridSx = {
+    "& .MuiDataGrid-columnHeaders": {
+      backgroundColor: "primary.main",
+      fontWeight: "bold",
+      fontSize: 14,
+      color: "white",
+    },
+    "& .MuiDataGrid-virtualScrollerContent": {
+      "& .MuiDataGrid-row": {
+        "&:nth-of-type(even)": {
+          backgroundColor: "grid.main",
+          // fontWeight: "bold",
+          // fontSize: 14,
+          // color: "white",
+        },
+      },
+    },
+  };
+
   const columns = (theme: Theme) => [
     {
       field: "name",
       headerName: "Name",
       minWidth: 150,
       renderCell: (cellValues: GridRenderCellParams<string>) => {
-        return cellValues.value;
+        return (
+          <Box
+            sx={{
+              color: "primary.main",
+              fontSize: 16,
+              fontWeight: "bold",
+            }}
+          >
+            {cellValues.value}
+          </Box>
+        );
       },
     },
     {
@@ -54,6 +88,22 @@ const ContactDataGrid = (props: Props) => {
         return cellValues.value;
       },
     },
+    {
+      field: "Print",
+      //headerName: "Work Preference",
+      minWidth: 150,
+      renderCell: (cellValues: GridRenderCellParams<string>) => {
+        return (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => handlePrintClick(cellValues)}
+          >
+            Print
+          </Button>
+        );
+      },
+    },
   ];
   const rows = [...contactData];
   //const rows =() => [...contactData];
@@ -66,6 +116,21 @@ const ContactDataGrid = (props: Props) => {
         headerHeight={60}
         rowHeight={80}
         //autoHeight
+        sx={dataGridSx}
+        components={{
+          Toolbar: () => (
+            <GridToolbar
+              sx={{
+                justifyContent: "flex-end",
+                "& button": { border: "none" },
+                "& .MuiBox-root": { display: "none" },
+              }}
+            ></GridToolbar>
+          ),
+        }}
+        initialState={{
+          sorting: { sortModel: [{ field: "name", sort: "asc" }] },
+        }}
       />
     </div>
   );
